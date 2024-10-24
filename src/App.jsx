1e3005506ffed89d609e95801b16f1727ae67c12
Surya Narayan Dash <surya.dash0967@gmail.com>
2024-10-24 19:26:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Hangman from './Components/Hangman'
+
+export default function App() {
+  return (
+    <>
+      <Hangman/>
+    </>
+  )
+}
---
+++ src/Components/Hangman.jsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react"
+
+export default function Hangman() {
+    const [word, setWord] = useState("");
+    const [len, setLen] = useState(Math.floor(Math.random() * 2) + 4);
+    const [guessedWord, setGuessedWord] = useState("");
+    const [choices, setChoices] = useState(5);
+    const [options, setOptions] = useState([]);
+    const [gameWon, setGameWon] = useState(false);
+
+
+    let URL = `https://random-word-api.herokuapp.com/word?length=${len}`
+
+    let getWord = async () => {
+        let response = await fetch(URL)
+        let data = await response.json()
+        setWord(data[0].toUpperCase())
+    }
+
+    let handleInputChange = (event) => {
+        setGuessedWord(event.target.value)
+    }
+
+    let handleSubmit = (event) => {
+        event.preventDefault()
+        if (guessedWord) {
+            if (guessedWord.toUpperCase() === word) {
+                setGameWon(true)
+            } else {
+                setChoices(choices - 1);
+                setGuessedWord("")
+            }
+        }
+    }
+
+    let handleOptionClick = (option) => {
+        setGuessedWord(guessedWord + option)
+    }
+
+
+    useEffect(() => {
+        getWord()
+    }, [])
+
+
+    useEffect(() => {
+        if (word) {
+            let shuffledOptions = [...word].sort(() => Math.random() - 0.5);
+            setOptions(shuffledOptions);
+            console.log(shuffledOptions)
+            console.log(word)
+        }
+    }, [word]);
+    return (
+        <>
+            <h2>Hangman Game | Guess the word</h2>
+            {
+                choices ?
+                    <>
+                        <div>
+                            <h3>Lives: {choices}</h3>
+                        </div>
+                        <h3>Options:</h3>
+                        {options.map((option) => {
+                            return (
+                                <button
+                                    style={{
+                                        margin: '10px 10px 50px 10px',
+                                        color: '#fff', fontSize: '17px'
+                                    }}
+                                    onClick={() => handleOptionClick(option)}
+                                >
+                                    {option.toUpperCase()}
+                                </button>
+                            )
+                        })}
+                        {
+                            !gameWon ?
+                                <form onSubmit={handleSubmit}>
+                                    <input
+                                        type="text"
+                                        onChange={handleInputChange}
+                                        value={guessedWord}
+                                        style={{
+                                            height: '1.5rem',
+                                            fontWeight: 'bold'
+                                        }}
+                                    />
+                                    <button type="submit">Submit</button>
+                                </form>
+                                :
+                                <>
+                                    <h3>Congratulations! You guessed right!</h3>
+                                    <h4>The correct word: {word}</h4>
+                                </>
+                        }
+                    </>
+
+                    :
+
+                    <>
+                        <h3>Game Over!</h3>
+                        <h4>The correct word: {word}</h4>
+                    </>
+            }
+        </>
+    )
+}
